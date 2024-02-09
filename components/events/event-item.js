7a615ef98b4b835ff15fa9202d45e1c7c5d5bc4e
lilyoh <lilyoh628@gmail.com>
2024-02-09 14:53:19 +1100

    add styling

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -2,6 +2,11 @@ import Link from 'next/link';
 
 import classes from './event-item.module.css';
 
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+
 function EventItem(props) {
 	const { title, image, date, location, id } = props;
 	const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
@@ -24,14 +29,21 @@ function EventItem(props) {
 					<h2>{title}</h2>
 				</div>
 				<div className={classes.date}>
+					<DateIcon />
 					<time>{humanReadableDate}</time>
 				</div>
 				<div className={classes.address}>
+					<AddressIcon />
 					<address>{formattedAddress}</address>
 				</div>
 			</div>
 			<div className={classes.actions}>
-				<Link href={exploreLink}>Explore Event</Link>
+				<Button link={exploreLink}>
+					<span>Explore More</span>
+					<span className={classes.icon}>
+						<ArrowRightIcon />
+					</span>
+				</Button>
 			</div>
 		</li>
 	);
