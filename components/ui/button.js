8bacdf56ee96ee8766f6a6183a67d2f5000d674a
lilyoh <lilyoh628@gmail.com>
2024-02-10 12:26:59 +1100

    add filter form

--- components/ui/button.js
+++ components/ui/button.js
@@ -3,12 +3,23 @@ import Link from 'next/link';
 import classes from './button.module.css';
 
 function Button(props) {
-	const { link, children } = props;
+	const { link, children, onClick } = props;
+
+	if (link) {
+		return (
+			<Link href={link}>
+				<a className={classes.btn}>{children}</a>
+			</Link>
+		);
+	}
 
 	return (
-		<Link href={link}>
-			<a className={classes.btn}>{children}</a>
-		</Link>
+		<button
+			className={classes.btn}
+			onClick={onClick}
+		>
+			{children}
+		</button>
 	);
 }
 
