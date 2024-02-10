13c2f622fdfb5f27a93c5deac99a80c726ad64c8
lilyoh <lilyoh628@gmail.com>
2024-02-10 12:44:12 +1100

    navigate filtered page with programmatic way

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,14 +1,32 @@
+import { useRef } from 'react';
+
 import Button from '../ui/button';
 
 import classes from './events-search.module.css';
 
-function EventsSearch() {
+function EventsSearch(props) {
+	const yearInputRef = useRef();
+	const monthInputRef = useRef();
+
+	function submitHandler(event) {
+		event.preventDefault();
+
+		const selectedYear = yearInputRef.current.value;
+		const selectedMonth = monthInputRef.current.value;
+
+		props.onSearch(selectedYear, selectedMonth);
+	}
+
 	return (
-		<form className={classes.form}>
+		<form
+			className={classes.form}
+			onSubmit={submitHandler}
+		>
 			<div className={classes.controls}>
 				<div className={classes.control}>
 					<label htmlFor="year"></label>
 					<select
+						ref={yearInputRef}
 						name="year"
 						id="year"
 					>
@@ -19,6 +37,7 @@ function EventsSearch() {
 				<div className={classes.control}>
 					<label htmlFor="month"></label>
 					<select
+						ref={monthInputRef}
 						name="month"
 						id="month"
 					>
